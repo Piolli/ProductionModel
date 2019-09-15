7a9b427bccbd483a4f2d2970086ccd129a668be7
Alexandr Kamyshev <sanek655@mail.ru>
2019-09-15 22:47:31 +0700

    Make data sources for terms and rules

--- Lab1_AI/ViewController.swift
+++ Lab1_AI/ViewController.swift
@@ -20,11 +20,19 @@ class ViewController: NSViewController {
     @IBOutlet weak var newRuleConditionTextField: NSTextField!
     @IBOutlet weak var newRuleResultTextField: NSTextField!
     
+    let terms = ["term1", "term2", "term3"]
+    let rules = [("condition1", "result1"), ("condition2", "result2"), ("condition3", "result3")]
+    let termsDS = TermsListDataSource(terms: ["term1", "term2", "term3"])
+    let rulesDS = RulesListDataSource(rules: [("condition1 & condition1 & condition1 & condition1 & condition1 & condition1", "result1"), ("condition2", "result2"), ("condition3", "result3")])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        rulesTableView.dataSource = rulesDS
+        rulesTableView.delegate = rulesDS
         
+        termsTableView.dataSource = termsDS
+        termsTableView.delegate = termsDS
     }
 
     override var representedObject: Any? {
@@ -47,3 +55,61 @@ class ViewController: NSViewController {
     }
 }
 
+//MARK: Rules
+class RulesListDataSource : NSObject, NSTableViewDataSource, NSTableViewDelegate {
+    
+    let rules: [(String, String)]
+    
+    init(rules: [(String, String)]) {
+        self.rules = rules
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return rules.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        let cell = NSTableCellView()
+        cell.textField?.cell?.title = rules[row].0
+        return cell
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: .init("cell"), owner: nil) as? NSTableCellView {
+            let cellText = tableColumn!.identifier.rawValue == "condition" ? rules[row].0 : rules[row].1
+            cell.textField?.cell?.title = cellText
+            return cell
+        }
+        
+        return nil
+    }
+}
+
+//MARK: Terms
+class TermsListDataSource : NSObject, NSTableViewDataSource, NSTableViewDelegate {
+    
+    let terms: [String]
+    
+    init(terms: [String]) {
+        self.terms = terms
+    }
+    
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return terms.count
+    }
+    
+    func tableView(_ tableView: NSTableView, objectValueFor tableColumn: NSTableColumn?, row: Int) -> Any? {
+        let cell = NSTableCellView()
+        cell.textField?.cell?.title = terms[row]
+        return cell
+    }
+    
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let cell = tableView.makeView(withIdentifier: .init("cell"), owner: nil) as? NSTableCellView {
+            cell.textField?.cell?.title = terms[row]
+            return cell
+        }
+        
+        return nil
+    }
+}
